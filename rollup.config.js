e3e28ec0f8c5d34a3461e5763acc9e9e5819631b
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2022-10-21 14:58:56 +0200

    Added linter

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-const uglify = require('@lopatnov/rollup-plugin-uglify');
-const resolve = require('@rollup/plugin-node-resolve');
-const typescript = require('@rollup/plugin-typescript');
+const uglify = require('@lopatnov/rollup-plugin-uglify')
+const resolve = require('@rollup/plugin-node-resolve')
+const typescript = require('@rollup/plugin-typescript')
 
 module.exports = [
-    {
-        input: 'src/index.ts',
-        output: {
-            file: 'dist/country-to-aws-region.min.js',
-            name: 'countryToAwsRegion',
-            format: 'umd',
-        },
-        plugins: [
-            resolve(),
-            typescript({ sourceMap: true }),
-            uglify(),
-        ],
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/country-to-aws-region.min.js',
+      name: 'countryToAwsRegion',
+      format: 'umd'
     },
-];
+    plugins: [
+      resolve(),
+      typescript({ sourceMap: true }),
+      uglify()
+    ]
+  }
+]
--- update-data.mjs
+++ update-data.mjs
@@ -1,62 +1,61 @@
-import fetch from 'node-fetch';
-import fs from 'fs';
-import path from 'path';
-import readline from 'readline';
-import stream from 'stream';
+import fetch from 'node-fetch'
+import fs from 'fs'
+import path from 'path'
+import readline from 'readline'
+import Stream from 'stream'
 
 const __dirname = path.resolve();
 
 (async () => {
-    console.log('Fetching data...');
+  console.log('Fetching data...')
 
-    const url = 'https://raw.githubusercontent.com/turnkeylinux/aws-datacenters/master/output/countries.index';
+  const url = 'https://raw.githubusercontent.com/turnkeylinux/aws-datacenters/master/output/countries.index'
 
-    // fetch url data to local file
-    const response = await fetch(url);
-    const data = await response.text();
-    const filePath = path.join(__dirname, 'countries.index');
-    fs.writeFileSync(filePath, data);
+  // fetch url data to local file
+  const response = await fetch(url)
+  const data = await response.text()
+  const filePath = path.join(__dirname, 'countries.index')
+  fs.writeFileSync(filePath, data)
 
-    // read every line from countries.index, separate by semicolon, remove the second item, log to console
-    const instream = fs.createReadStream(path.join(__dirname, 'countries.index'));
-    const outstream = new stream;
-    const rl = readline.createInterface(instream, outstream);
+  // read every line from countries.index, separate by semicolon, remove the second item, log to console
+  const instream = fs.createReadStream(path.join(__dirname, 'countries.index'))
+  const outstream = new Stream()
+  const rl = readline.createInterface(instream, outstream)
 
-    console.log('Parsing data...');
+  console.log('Parsing data...')
 
-    let countryRegionData = {};
+  const countryRegionData = {}
 
-    for await (const line of rl) {
-        const lineData = line.split(';');
+  for await (const line of rl) {
+    const lineData = line.split(';')
 
-        const countryCode = lineData[0];
+    const countryCode = lineData[0]
 
-        let regions = [];
+    let regions = []
 
-        const primaryRegion = lineData[2];
+    const primaryRegion = lineData[2]
 
-        if (primaryRegion) {
-            regions.push(primaryRegion);
-        }
-
-        const secondaryRegionsString = lineData[3];
-
-        if(secondaryRegionsString !== 'None') {
-            regions = secondaryRegionsString.split(',');
-        }
-
-        // make sure regions are unique
-        regions = [...new Set(regions)];
-
-        countryRegionData[countryCode] = regions;
+    if (primaryRegion) {
+      regions.push(primaryRegion)
     }
 
-    // write data to file
-    console.log('Writing data...');
+    const secondaryRegionsString = lineData[3]
 
-    const dataFilePath = path.join(__dirname, 'src', 'data.ts');
-    fs.writeFileSync(dataFilePath, '// AUTO-GENERATED DATA\n// This file should only be updated with the `npm run update-data` command.\n\nexport const countryRegionData: Record<string, string[]> = ' + JSON.stringify(countryRegionData, null, 4));
+    if (secondaryRegionsString !== 'None') {
+      regions = secondaryRegionsString.split(',')
+    }
 
-    console.log('Done! Data written to src/data.json');
-})();
+    // make sure regions are unique
+    regions = [...new Set(regions)]
 
+    countryRegionData[countryCode] = regions
+  }
+
+  // write data to file
+  console.log('Writing data...')
+
+  const dataFilePath = path.join(__dirname, 'src', 'data.ts')
+  fs.writeFileSync(dataFilePath, '// AUTO-GENERATED DATA\n// This file should only be updated with the `npm run update-data` command.\n\nexport const countryRegionData: Record<string, string[]> = ' + JSON.stringify(countryRegionData, null, 4))
+
+  console.log('Done! Data written to src/data.json')
+})()
